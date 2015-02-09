d19fd3f9df1fa730d5f2a8a6891a58536152cac6
Topstitch <kallen@spoonflower.com>
2015-02-09 16:25:02 -0500

    Got rid of icky grades

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post 'login/changepassword'
   get 'login/changepassword'
 
-  resources :grades
+  
   resources :parents
   resources :teachers
   resources :students
---
+++ db/migrate/20150209210601_add_achievements.rb
@@ -0,0 +1,14 @@
+class AddAchievements < ActiveRecord::Migration
+  def change
+    create_table :achievements do |t|
+      t.string :description
+      t.integer :points
+      t.integer :teacher_id
+
+      t.timestamps null: false
+    end
+    
+    drop_table :grades
+  end
+
+end
